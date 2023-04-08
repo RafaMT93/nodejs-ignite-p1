54a72c88b7836a4f961cbec135d6689f3a2a27f5
Rafael Moraes <rafael.moraes@ilog.com.br>
2023-04-08 10:25:41 -0300

    Fix: Regex adjust in utils

--- src/utils/buid-route-path.js
+++ src/utils/buid-route-path.js
@@ -1,6 +1,6 @@
 export function buildRoutePath(path) {
     const regex = /:({a-zA-Z}+)/g;
-    const pathWithRegex = path.replaceAll(regex, "([a-z0-9\-_]+)");
+    const pathWithRegex = path.replaceAll(regex, "(?<$1>[a-z0-9\-_]+)");
 
     const pathRegex = new RegExp(`^${pathWithRegex}(?<query>\\?(.*))?$`)
 
